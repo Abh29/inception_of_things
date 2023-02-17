5faeaea8a31dae0958b53ba8ecada08548d0ffe3
Abh29 <aboutifor@stud.kpfu.ru>
2023-02-18 00:55:06 +0300

    updationg p3

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#create a cluster
+k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8888:8888@loadbalancer --port 8443:443@loadbalancer
+
 #start the cluster
 #k3d cluster start dev-cluster
 
@@ -11,9 +14,10 @@ kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 #port forwarding
-kubectl port-forward svc/argocd-server -n argocd 8081:443 &
+kubectl port-forward svc/argocd-server -n argocd 8081:443 2>/dev/null &
 
 #get password
+echo "passworld: "
 kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | cat && echo
 
 
