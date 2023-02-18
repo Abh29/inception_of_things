7c9b788fe3f3a7d26b9c7e646c28ca7eb6233ece
Abh29 <aboutifor@stud.kpfu.ru>
2023-02-18 14:03:57 +0300

    adding bonus part

---
+++ bonus/scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "setup..."
+
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates perl
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="http://iot.mehill.42" apt-get install gitlab-ce
