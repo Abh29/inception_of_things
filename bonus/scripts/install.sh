5034f1b470ed51ddabd8a202ec4aae2251e937b2
Abh29 <aboutifor@stud.kpfu.ru>
2023-02-18 20:03:54 +0300

    adding bonus

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -6,3 +6,5 @@ sudo apt-get update
 sudo apt-get install -y curl openssh-server ca-certificates perl
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="http://iot.mehill.42" apt-get install gitlab-ce
+
+
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -7,8 +7,7 @@ k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8888:8888@load
 k3d cluster start dev-cluster
 
 #create spacenames
-kubectl create namespace argocd
-kubectl create namespace dev
+kubectl create namespace gitlab
 
 #install argocd in the cluster
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -19,7 +18,5 @@ kubectl port-forward svc/argocd-server -n argocd 8081:443 2>/dev/null &
 #get password
 echo "passworld for admin: "
 kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | cat && echo
-
-
 
 
